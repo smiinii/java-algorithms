ecfdc1e0554c50ad80c5e8c8752da6466b1b481a
smiinii <sm020320@naver.com>
2026-03-17 14:34:04 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {0, 0};
+        for (int num : num_list) {
+            if (num % 2 == 0) {
+                answer[0]++;
+            }
+            if (num % 2 != 0) {
+                answer[1]++;   
+            }
+        }
+        return answer;
+    }
+}
