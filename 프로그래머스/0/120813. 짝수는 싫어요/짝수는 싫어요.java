0192fc9be7ba85d6aee7b98abec1727b84850baa
smiinii <sm020320@naver.com>
2026-03-17 16:47:26 +0900

    [level 0] Title: 짝수는 싫어요, Time: 2.84 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            if(i % 2 != 0) {
+                answer.add(i);
+            }
+        }
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
