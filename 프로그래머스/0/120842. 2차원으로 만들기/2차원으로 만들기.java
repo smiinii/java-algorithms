d4b7b3446384aab9806934f1e6a1c133464cd99e
smiinii <sm020320@naver.com>
2026-03-22 17:58:43 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.03 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public int[][] solution(int[] num_list, int n) {
+        int[][] answer = new int[num_list.length / n][n];
+        for (int i = 0; i < num_list.length; i += n) {
+            answer[i / n] = Arrays.copyOfRange(num_list, i, i + n);
+        }
+        return answer;
+    }
+}
