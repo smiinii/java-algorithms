d401acf4ad8fb84e75b056e9eaa0f52d4790413b
smiinii <sm020320@naver.com>
2026-05-20 23:53:34 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 5.04 ms, Memory: 65.8 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -3,13 +3,13 @@ class Solution {
         long answer = 0;
         long temp = 1;
         while(true) {
+            if (temp * temp > n) {
+                break;
+            } 
             if (temp * temp == n) {
                 return (temp + 1) * (temp + 1);
             }
             temp++;
-            if (temp * temp > n) {
-                break;
-            } 
         }
         return -1;
     }
