a89b9f59211a45ebd805f3b3f7a1a25b71e3bf0b
smiinii <sm020320@naver.com>
2026-06-04 11:07:52 +0900

    [level 1] Title: 예산, Time: 2.94 ms, Memory: 66.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int temp = 0;
+        Arrays.sort(d);
+        
+        for(int i = 0; i < d.length; i++) { 
+            temp += d[i];
+            if(temp > budget) {
+                break;
+            }
+            answer++;
+        }
+        
+        return answer;
+    }
+}
