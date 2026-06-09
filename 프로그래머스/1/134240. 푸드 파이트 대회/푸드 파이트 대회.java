392cd44f3cd8f32c49edbba28fbc2d96274ce6da
smiinii <sm020320@naver.com>
2026-06-09 17:41:35 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.97 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,25 +1,15 @@
 class Solution {
     public String solution(int[] food) {
         StringBuilder sb = new StringBuilder();
-        int[] arr = new int[food.length];
-        for(int i = 0; i < food.length; i++) {
-            if(food[i] % 2 != 0) {
-                arr[i] = food[i] - 1;
-            } else if(food[i] % 2 == 0) {
-                arr[i] = food[i];
-            } else if (food[i] == 0) {
-                arr[i] = food[i];
-            }
-        }
         
-        for(int i = 1; i < arr.length; i++) {
-            for(int j = 0; j < arr[i] / 2; j++) {
+        for(int i = 1; i < food.length; i++) {
+            for(int j = 0; j < food[i] / 2; j++) {
                 sb.append(i);
             }
         }
         sb.append("0");
-        for(int i = arr.length - 1; i >= 1; i--) {
-            for(int j = 0; j < arr[i] / 2; j++) {
+        for(int i = food.length - 1; i >= 1; i--) {
+            for(int j = 0; j < food[i] / 2; j++) {
                 sb.append(i);
             }
         }
