03e4caf056f91a812760d05426a97aeb9e1a5a9a
smiinii <sm020320@naver.com>
2026-06-11 15:05:56 +0900

    [level 1] Title: 명예의 전당 (1), Time: 1.76 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] result = new int[score.length];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for (int i = 0; i < score.length; i++) {
+            pq.add(score[i]);
+            
+            if (pq.size() > k) {
+                pq.poll();
+            }
+            
+            result[i] = pq.peek();
+        }
+        
+        return result;
+    }
+}
